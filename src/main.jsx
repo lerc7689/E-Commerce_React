7764dfb5541e6873ebbd535a75b61a75800b098b
Luis Ramirez <lerc7689@hotmail.com>
2023-06-09 13:16:15 -0400

    second

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
-import router from "./router/index.jsx"
-import { Provider } from 'react-redux'
+import {router} from "./router"
+import { Provider } from 'react-redux';
 import store from './store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
